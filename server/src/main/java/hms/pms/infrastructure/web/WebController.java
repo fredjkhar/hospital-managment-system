8e3bdc1fe9c525fa6ec04048cbde23cf4f4cfb27
fredjkhar <fredj.kharroubi2@gmail.com>
2023-12-06 11:16:47 -0500

    HMS-48 add the rest of controllers

--- server/src/main/java/hms/pms/infrastructure/web/WebController.java
+++ server/src/main/java/hms/pms/infrastructure/web/WebController.java
@@ -1,11 +1,14 @@
 package hms.pms.infrastructure.web;
 
 import hms.pms.application.dtos.queries.*;
+import hms.pms.application.dtos.responses.PatientFileViewDTO;
+import hms.pms.application.dtos.responses.WardViewDTO;
 import hms.pms.domain.ward.facade.WardFacade;
 import hms.pms.infrastructure.services.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,75 +24,83 @@ public class WebController {
 
     @PostMapping("/admitPatient/{wardId}")
     @ResponseBody
-    public Boolean admitPatient(@RequestBody AdmissionCreateDTO admissionCreateDTO, @RequestParam(value = "wardId") UUID wardId) {
+    public Boolean admitPatient(@RequestBody AdmissionCreateDTO admissionCreateDTO, @PathVariable UUID wardId) {
         return services.admitPatient(wardId, admissionCreateDTO);
     }
 
+
     @PostMapping("/admitPatientFromRequestList/{wardId}")
     @ResponseBody
-    public Boolean admitPatientFromRequestList(@RequestBody AdmissionRequestCreateDTO admissionRequestCreateDTO, @RequestParam(value = "wardId") UUID wardId) {
+    public Boolean admitPatientFromRequestList(@RequestBody AdmissionRequestCreateDTO admissionRequestCreateDTO, @PathVariable UUID wardId) {
         return services.admitPatientFromRequestList(wardId, admissionRequestCreateDTO);
     }
 
     @GetMapping("/consultPatientFile/{patientId}")
-    @ResponseBody
-    public Boolean consultPatientFile(@RequestParam(value = "patientId") UUID patientId) {
+    public PatientFileViewDTO consultPatientFile(@PathVariable UUID patientId) {
         return services.consultPatientFile(patientId);
     }
 
     @PostMapping("/dischargePatient/{wardId}")
     @ResponseBody
-    public Boolean dischargePatient(@RequestBody DischargeCreateDTO dischargeInfo, @RequestParam(value = "wardId") UUID wardId) {
+    public Boolean dischargePatient(@RequestBody DischargeCreateDTO dischargeInfo, @PathVariable UUID wardId) {
         return services.dischargePatient(wardId, dischargeInfo);
     }
 
-    @PostMapping("/addPatientToRequestList")
+    @PostMapping("/prescribeMedication/{patientId}")
     @ResponseBody
-    public String addPatientToRequestList(@RequestBody AdmissionRequestCreateDTO admissionRequestCreateDTO) {
-        return "Nice";
+    public Boolean prescribeMedication(@RequestBody PrescriptionCreateDTO prescriptionCreateDTO, @PathVariable UUID patientId) {
+        return services.prescribeMedication(patientId, prescriptionCreateDTO);
     }
-    
+
     @PostMapping("/registerPatient")
     @ResponseBody
-    public String registerPatient(@RequestBody PatientInfoCreateDTO patientInfoCreateDTO) {
-        return "Nice";
-    }
-
-    @GetMapping("/getPatientsList")
-    public String getPatientsList() {
-        return "Nice";
-    }
-
-    @PostMapping("/updatePatientFile/{patientId}")
-    @ResponseBody
-    public String updatePatientFile(@RequestParam(value = "patientId") UUID patientId, @RequestBody PatientInfoCreateDTO patientInfoCreateDTO) {
-        return "Nice";
-    }
-
-    @PostMapping("/updateStaff/{staffId}")
-    @ResponseBody
-    public String updateStaff(@RequestParam(value = "staffId") UUID staffId, @RequestBody StaffInfoCreateDTO staffInfoCreateDTO) {
-        return "Nice";
+    public Boolean registerPatient(@RequestBody PatientInfoCreateDTO patientInfoCreateDTO) {
+        return services.registerPatient(patientInfoCreateDTO);
     }
 
     @PostMapping("/registerStaff")
     @ResponseBody
-    public String registerStaff(@RequestBody StaffInfoCreateDTO staffInfoCreateDTO) {
-        return "Nice";
+    public Boolean registerStaff(@RequestBody StaffInfoCreateDTO staffInfoCreateDTO) {
+        return services.registerStaff(staffInfoCreateDTO);
+    }
+
+    @PostMapping("/requestPatientAdmission/{wardId}")
+    @ResponseBody
+    public Boolean requestPatientAdmission(@RequestBody AdmissionRequestCreateDTO admissionRequestCreateDTO, @PathVariable UUID wardId) {
+        return services.requestPatientAdmission(admissionRequestCreateDTO, wardId);
+    }
+
+    @PostMapping("/updatePatientFile/{patientId}")
+    @ResponseBody
+    public Boolean updatePatientFile(@RequestBody PatientInfoCreateDTO patientInfoCreateDTO, @PathVariable UUID patientId) {
+        return services.updatePatientFile(patientId, patientInfoCreateDTO);
+    }
+
+    @GetMapping("/getWard/{wardId}")
+    public WardViewDTO visualizeWard(@RequestParam(value = "wardId") UUID wardId) {
+        return services.visualizeWard(wardId);
+    }
+
+    @PostMapping("/updateStaff/{staffId}")
+    @ResponseBody
+    public Boolean updateStaff(@PathVariable UUID staffId, @RequestBody StaffInfoCreateDTO staffInfoCreateDTO) {
+        return services.updateStaff(staffId, staffInfoCreateDTO);
+    }
+
+    @GetMapping("/getPatientsList")
+    public List<PatientFileViewDTO> getPatientsList() {
+        return services.getPatientsList();
     }
 
     @GetMapping("/loginStaff/{staffId}")
-    public String loginStaff() {
+    public String loginStaff(@PathVariable UUID staffId) {
+        //TODO: Aymane
         return "Zween";
     }
 
     @GetMapping("/logoutStaff/{staffId}")
-    public String logoutStaff() {
-        return "Zween";
-    }
-
-    @GetMapping("/getWard/{wardId}")
-    public String getWard(@RequestParam (value = "wardId") UUID wardId) {
+    public String logoutStaff(@PathVariable UUID staffId) {
+        //TODO: Aymane
         return "Zween";
     }
 }
